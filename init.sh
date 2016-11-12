6a42c32ff32496019aeb5f6af88caad343e2dc91
Oleksandr Lisovyi <sansan14@mail.ru>
2016-11-12 21:03:29 +0000

    last ver

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
 
-sudo ln -s /home/oleksandr/Public/web2/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -s /home/oleksandr/Public/web2/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
